2efb94ca8c5ccf01522ffb212552f94d48b229c3
Lidiya Lesnikova <lesnikoval@yandex.ru>
2024-07-10 20:22:27 +0300

    dz 5 prepared

--- 5/Tasks.py
+++ 5/Tasks.py
@@ -2,6 +2,7 @@ from typing import Optional
 from pydantic import BaseModel
 
 class Task(BaseModel):
+    id: int
     title: str
     description: Optional[str] = None
     status: bool #True - выполнена, False - не выполнена
