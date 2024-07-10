d424cb9e38ec13dac40b1f6b519256319e656c09
Lidiya Lesnikova <lesnikoval@yandex.ru>
2024-07-10 19:44:47 +0300

    dz 5 prepared

--- 5/task5.py
+++ 5/task5.py
@@ -12,43 +12,63 @@ API должен содержать следующие конечные точк
 Для этого использовать библиотеку Pydantic.
 '''
 
-import logging
 from fastapi import FastAPI
+import uvicorn
+
 from Tasks import Task
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 app = FastAPI()
+tasks = []
 
 @app.get("/")
 async def read_root():
-    logger.info('Отработал GET запрос.')
-    return {"Hello": "World"}   
+    return "Hello"
 
 @app.get("/tasks/")
 async def return_all_tasks():
-    logger.info('Отработал GET запрос.')
-    return {"Список ": "World"}   
+    return tasks
 
 @app.get("/tasks/{id}")
-async def return_task(id: int, task: Task):
-    logger.info(f'Отработал PUT запрос для item id = {id}.')
-    return {"id": id, "task": task}
+async def return_task(id: int):
+    select = [task for task in tasks if task.id==id]
+    if len(select)==1:
+        select_task = select[0]
+        return {"id": id, "task": select_task}
+    else:
+        return f"нет Задания с id {id}"
 
 @app.post("/tasks/")
 async def create_task(task: Task):
-    logger.info('Отработал POST запрос.')
+    task = Task(id=len(tasks)+1, title=task.title, description=task.description, status=False)
+    tasks.append(task)
     return task
 
 @app.put("/tasks/{id}")
 async def update_task(id: int, task: Task):
-    logger.info(f'Отработал PUT запрос для item id = {id}.')
-    return {"id": id, "task": task}
+    select_task = [task for task in tasks if task.id==id]
+    if len(select_task)==1:
+        index = tasks.index(select_task[0])
+        current_task = Task(id=id, title=task.title, description=task.description, status=task.status)
+        tasks[index] = current_task
+        return current_task
+    else:
+        return f"нет Задания с id {id}"
 
 
 @app.delete("/tasks/{id}")
 async def delete_task(id: int):
-    logger.info(f'Отработал DELETE запрос для item id = {id}.')
-    return {"id": id}
+    select_task = [task for task in tasks if task.id==id]
+    if len(select_task)==1:
+        index = tasks.index(select_task[0])
+        tasks.pop(index)
+        return f"Задание с id {id} удалено"
+    else:
+        return f"нет Задания с id {id}"
 
-
+if __name__ == "__main__":
+    uvicorn.run(
+        'task5:app',
+        host='127.0.0.1',
+        port=8000,
+        reload=True
+    )
